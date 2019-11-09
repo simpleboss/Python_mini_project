f629a8fb36c69c721160724a842a90e99c8b95da
gichan lee <gichan.lee@threemindev.com>
2019-11-09 12:16:35 +0700

    insert get keyword at each function name

--- sals_shipping.py
+++ sals_shipping.py
@@ -1,4 +1,4 @@
-def cost_of_ground_shipping(weight):
+def get_cost_of_ground_shipping(weight):
     cost = 20
     if weight <= 2:
         per_pound = 1.5
@@ -14,7 +14,7 @@ cost_of_premium_ground_shipping = 125
 
 #print(cost_of_ground_shipping(8.4))
 
-def cost_of_drone_shipping(weight):
+def get_cost_of_drone_shipping(weight):
     cost = 0
     if weight <= 2:
         per_pound = 4.5
@@ -28,20 +28,20 @@ def cost_of_drone_shipping(weight):
 
 #print(cost_of_drone_shipping(1.5))
 
-def best_way_of_shipping(weight):
-    if (cost_of_premium_ground_shipping <= cost_of_drone_shipping(weight) and
-    cost_of_premium_ground_shipping <= cost_of_ground_shipping(weight) ):
+def get_best_way_of_shipping(weight):
+    if (cost_of_premium_ground_shipping <= get_cost_of_drone_shipping(weight) and
+    cost_of_premium_ground_shipping <= get_cost_of_ground_shipping(weight) ):
         result_of_way_of_shipping = 'Premium Ground shipping'
         result_of_price = cost_of_premium_ground_shipping
-    elif(cost_of_drone_shipping(weight) <= cost_of_ground_shipping(weight)):
+    elif(get_cost_of_drone_shipping(weight) <= get_cost_of_ground_shipping(weight)):
         result_of_way_of_shipping = 'Drone shipping'
         result_of_price = cost_of_drone_shipping(weight)
     else:
         result_of_way_of_shipping = 'Ground shipping'
-        result_of_price = cost_of_ground_shipping(weight)    
+        result_of_price = get_cost_of_ground_shipping(weight)    
     print('The best way of shipping :'+result_of_way_of_shipping) 
     print('The price                :'+str(result_of_price))  
 
-best_way_of_shipping(4.8)
+get_best_way_of_shipping(4.8)
 print('=====')
-best_way_of_shipping(41.5)
+get_best_way_of_shipping(41.5)
