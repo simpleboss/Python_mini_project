d3799f9ba2a440d11e9014a9a07e3e2250f2b1d3
Gichan Lee <gichan.lee@threemindev.com>
2019-11-12 16:54:11 +0700

    modify task1 code

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -11,28 +11,34 @@ email_four = open("email_four.txt", "r").read()
 
 # Task 2
 print('Task 2')
-email_one_list_by_word = email_one.split(' ')
-email_one_censored_all_instances_of_the_phrase_learning_algorithms = []
-i = 0
-while i < len(email_one_list_by_word):
-    if 'learning' in email_one_list_by_word[i].lower():
-        if 'algorithm' in email_one_list_by_word[i+1].lower():
-            email_one_censored_all_instances_of_the_phrase_learning_algorithms.append('CENSORED')
-            i += 2
+def get_email_censored_learning_algorithm(email):
+    email_list_by_word = email.split(' ')
+    email_censored_learning_algorithm = []
+    i = 0
+    while i < len(email_list_by_word):
+        if 'learning' in email_list_by_word[i].lower():
+            if 'algorithm' in email_list_by_word[i+1].lower():
+                email_censored_learning_algorithm.append('CENSORED')
+                i += 2
+            else:
+                email_censored_learning_algorithm.append(email_list_by_word[i])
+                i += 1
         else:
-            email_one_censored_all_instances_of_the_phrase_learning_algorithms.append(email_one_list_by_word[i])
+            email_censored_learning_algorithm.append(email_list_by_word[i])
             i += 1
-    else:
-        email_one_censored_all_instances_of_the_phrase_learning_algorithms.append(email_one_list_by_word[i])
-        i += 1
-#print(' '.join(email_one_censored_all_instances_of_the_phrase_learning_algorithms))
+    return ' '.join(email_censored_learning_algorithm)
 
 
-# Task 3
+print(get_email_censored_learning_algorithm(email_one))
+
+
+    # Task 3
+
+
 print('Task 3')
 
 
-def getting_censor_list_of_words_and_phrases(email, words_to_censor):
+def get_censor_list_of_words_and_phrases(email, words_to_censor):
     email_list_by_word = email.split(' ')
     email_censored = []
     i = 0
@@ -57,32 +63,20 @@ def getting_censor_list_of_words_and_phrases(email, words_to_censor):
 
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
-print(getting_censor_list_of_words_and_phrases(email_two, proprietary_terms))
+print(get_censor_list_of_words_and_phrases(email_two, proprietary_terms))
 
 
-# # Task 4
-# print('Task 4')
-#
-#
-# def getting_censor_negative_words_twice(email, negative_words):
-#     email_censored = getting_censor_list_of_words_and_phrases(email, proprietary_terms)
-#     email_censored_list_by_sentence = email_censored.split('\n')
-#     count = 0
-#     email_censored_and_removed_negative_words_list = []
-#     for email_sentence in email_censored_list_by_sentence:
-#         for negative_word in negative_words:
-#             if negative_word in email_sentence:
-#                 count += 1
-#                 if count >= 3:
-#                     break
-#                 elif email_sentence not in email_censored_and_removed_negative_words_list:
-#                     email_censored_and_removed_negative_words_list.append(email_sentence)
-#     return '\n'.join(email_censored_and_removed_negative_words_list)
-#
-#
-# negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
-# print(getting_censor_negative_words_twice(email_three, negative_words))
-#
-#
+# Task 4
+print('Task 4')
+
+
+def get_censor_after_negative_words_twice(email, words_to_censor):
+    pass
+
+
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help",
+                      "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed",
+                      "distressed", "concerning", "horrible", "horribly", "questionable"]
+print(get_censor_after_negative_words_twice(email_three, negative_words))
 # # Task 5
 # print('Task 5')
