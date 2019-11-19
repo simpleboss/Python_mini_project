c2539c64244d1cd13c2b560059a96649e49f2ac4
Gichan Lee <gichan.lee@threemindev.com>
2019-11-19 15:29:57 +0700

    fix syntax of assert

--- Delete Starting Even Numbers.py
+++ Delete Starting Even Numbers.py
@@ -53,3 +53,4 @@ for i in range(len(test_case_question)):
 # Uncomment the lines below when your function is done
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
+
