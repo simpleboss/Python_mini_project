45a246f82a4263664c5251e866000913741acb10
Gichan Lee <gichan.lee@threemindev.com>
2019-11-19 23:02:31 +0700

    complete task 15

--- scrabble.py
+++ scrabble.py
@@ -26,7 +26,7 @@ brownie_points = score_word("BROWNIE")
 print(brownie_points)
 
 # Task 9
-player_to_words = player_to_words = {
+player_to_words = {
     "player1":["BLUE","TENNIS","EXIT"],
     "wordNerd":["EARTH", "EYES", "MACHINE"],
     "Lexi Con":["ERASER", "BELLY", "HUSKY"],
@@ -47,3 +47,28 @@ for player, words in player_to_words.items():
 
 # Task 14
 print(player_to_points)
+
+# Task 15
+def play_word(player, word):
+    player_to_words[player].append(word)
+
+play_word('player1', 'APPLE')
+print(player_to_words['player1'])
+
+
+def update_point_totals(player_to_words):
+    player_to_points = {}
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+        player_to_points[player] = str(player_points)
+    return player_to_points
+
+
+print(update_point_totals(player_to_words))
+
+
+letter_to_points.update({key.lower(): value for key, value in zip(letters, points)})
+play_word('player1', 'hanoi')
+print(update_point_totals(player_to_words))
