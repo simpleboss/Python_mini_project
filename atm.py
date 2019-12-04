4743a9bef5f7921e309fb8baf905e4638e021752
Gichan Lee <gichan.lee@threemindev.com>
2019-12-04 18:52:34 +0700

    input

--- atm.py
+++ atm.py
@@ -1,12 +1,13 @@
-p_list = [3, 1, 4, 3, 2]
-n = len(p_list)
+n = int(input())
+p_list = input().split(' ')
+
 
 def total_time(p_list):
     new_list = sorted(p_list)
     remained_n = n
     result = 0
     for p in new_list:
-        result += remained_n * p
+        result += remained_n * int(p)
         remained_n -= 1
     return result
 
