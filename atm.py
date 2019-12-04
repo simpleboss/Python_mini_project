5c2ba135fc06eea7758d287d341dd894c1e8424d
Gichan Lee <gichan.lee@threemindev.com>
2019-12-04 18:44:40 +0700

    loop in new_list

--- atm.py
+++ atm.py
@@ -1,11 +1,11 @@
-p_list = [1]
+p_list = [3, 1, 4, 3, 2]
 n = len(p_list)
 
 def total_time(p_list):
     new_list = sorted(p_list)
     remained_n = n
     result = 0
-    for p in p_list:
+    for p in new_list:
         result += remained_n * p
         remained_n -= 1
     return result
