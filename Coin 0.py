3f40959b63036dfa9705b6555550416df15fe12f
Gichan Lee <gichan.lee@threemindev.com>
2019-12-07 12:06:34 +0700

    use reversed(a_list)

--- Coin 0.py
+++ Coin 0.py
@@ -1,18 +1,20 @@
-# n = 10
-n = int(input())
-# k = 4790
-k = int(input())
-# a_list = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000]
-a_list = []
-for i in range(n):
-    a_list.append(int(input()))
+n = 10
+k = 4790
+a_list = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000]
+
+# n = int(input())
+# k = int(input())
+# a_list = []
+# for i in range(n):
+#     a_list.append(int(input()))
 
 
 def minimum_number_of_coins(k, a_list):
     result = 0
-    for a in range(len(a_list)-1 ,-1,-1):
-        result += k // a_list[a]
-        k = k % a_list[a]
+    a_list_reversed = reversed(a_list)
+    for a in a_list_reversed:
+        result += k // a
+        k = k % a
     return result
 
 
